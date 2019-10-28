18a75020a086299e3748e85d4a77ebe04db67148
hardyyang74 <4976691@qq.com>
2019-10-28 11:32:37 +0800

    add file: demo.py

---
+++ python/demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import socket
+
+def get_host_ip():
+  """get address of localhost"""
+  try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    ip = s.getsockname()[0]
+  finally:
+    s.close()
+
+  return ip
+
+if __name__ == "__main__":
+  print(get_host_ip())
